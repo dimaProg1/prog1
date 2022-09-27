8d2e05f4567cefc036eae13a861317952726cfcc
dimaProg1 <114497676+dimaProg1@users.noreply.github.com>
2022-09-27 17:10:04 +0300

    Add files via upload

---
+++ site_prog.js
@@ -0,0 +1,15 @@
+﻿let body = document.querySelector('body')
+let button = document.querySelector('#button')
+let text = document.querySelector('#dev')
+let list1 = ['Баитов Иван', 'Баркалов Роман', 'Воробьёв Никита', 'Гарстуков Никита', 'Збутевич Элина', 'Кириченко Кирилл', 'Константинов Григорий', 'Окс Елизавета', 'Корошавин Александр', 'Нмбуренко Александр', 'Нмбуренко Дарья']
+button.addEventListener('click', ClickEVent)
+
+function ClickEVent() {
+    let texted = prompt('Введите цифру в списке для выбора класса: 1 - 7Б')
+    let x = 0
+    if (texted == 1) {
+       x = Math.round(Math.random() * 11)
+       alert(list1[x])
+    }
+    
+}
